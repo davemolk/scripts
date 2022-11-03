ba2e510cde3c7b93eb009572a54389529d3e9b17
Dave Molk <davemolk@gmail.com>
2022-11-02 19:20:28 -0600

    lines: add csv option

--- lines/main.go
+++ lines/main.go
@@ -2,8 +2,10 @@ package main
 
 import (
 	"bufio"
+	"encoding/csv"
 	"flag"
 	"fmt"
+	"io"
 	"log"
 	"os"
 	"strings"
@@ -12,51 +14,80 @@ import (
 
 func main() {
 	var dir string
-	flag.StringVar(&dir, "", "", "directory to read files from")
-
-	files, err := readDir(dir)
+	var ext string
+	flag.StringVar(&dir, "dir", "", "directory to read files from")
+	flag.StringVar(&ext, "ext", "txt", "file extension (csv or txt")
+	flag.Parse()
+	
+	files, err := readDir(dir, ext)
 	if err != nil {
 		log.Fatal(err)
 	}
 
-	var allChan []<-chan string
+	var inputChans []<-chan []string
 	for _, file := range files {
-		ch, err := readFile(file)
+		ch, err := readFile(file, ext)
 		if err != nil {
 			fmt.Println(err)
 			continue
 		}
-		allChan = append(allChan, ch)
+		inputChans = append(inputChans, ch)
 	}
 
 	exit := make(chan struct{})
-	chM := merge(allChan)
+	mergeCh := merge(inputChans)
 
 	go func() {
 		defer close(exit)
-		f, err := os.Create("results.txt")
-		if err != nil {
-			fmt.Println(err)
-			return	
-		}
-		defer f.Close()
-		for v := range chM {
-			fmt.Fprintf(f, "%s\n", v)
+		switch {
+			case ext == "txt":
+				writeTxt(mergeCh)
+			case ext == "csv":
+				writeCSV(mergeCh)
 		}
 	}()
 
 	<-exit
-	fmt.Println("all done")
+	fmt.Println("all done!")
 }
 
-func merge(cs []<-chan string) <-chan string {
-	var wg sync.WaitGroup
-	out := make(chan string)
+func writeTxt(mergeCh <-chan []string) {
+	f, err := os.Create("results.txt")
+	if err != nil {
+		fmt.Println(err)
+		return	
+	}
+	defer f.Close()
+	for v := range mergeCh {
+		fmt.Fprintf(f, "%s\n", v[0])
+	}
+}
 
-	send := func(c <-chan string) {
+func writeCSV(mergeCh <-chan []string) {
+	f, err := os.Create("results.csv")
+	if err != nil {
+		fmt.Println(err)
+		return	
+	}
+	defer f.Close()
+	w := csv.NewWriter(f)
+	defer w.Flush()
+
+	var data [][]string
+	for v := range mergeCh {
+		row := v
+		data = append(data, row)
+	}
+	w.WriteAll(data)
+}
+
+func merge(cs []<-chan []string) <-chan []string {
+	var wg sync.WaitGroup
+	out := make(chan []string)
+
+	send := func(c <-chan []string) {
 		defer wg.Done()
 		for n := range c {
-			fmt.Println(n)
 			out <- n
 		}
 	}
@@ -75,7 +106,7 @@ func merge(cs []<-chan string) <-chan string {
 	return out
 }
 
-func readDir(dir string) ([]string, error) {
+func readDir(dir, ext string) ([]string, error) {
 	if dir == "" {
 		wd, err := os.Getwd()
 		if err != nil {
@@ -91,33 +122,50 @@ func readDir(dir string) ([]string, error) {
 
 	var names []string
 	for _, file := range files {
-		if strings.HasSuffix(file.Name(), ".txt") {
+		if strings.HasSuffix(file.Name(), ext) {
 			names = append(names, file.Name())
 		}
 	}
 	return names, nil
 }
 
-func readFile(file string) (<-chan string, error) {
+func readFile(file, ext string) (<-chan []string, error) {
 	f, err := os.Open(file)
 	if err != nil {
 		return nil, fmt.Errorf("read error: %w", err)
 	}
 
-	ch := make(chan string)
-	scanner := bufio.NewScanner(f)
+	ch := make(chan []string)
 
-	go func() {
-		for scanner.Scan() {
-			ch <- scanner.Text()
-			if err := scanner.Err(); err != nil {
-				fmt.Printf("error for %s: %v\n", file, err)
-				break
+	switch {
+	case ext == "csv":
+		c := csv.NewReader(f)
+		go func() {
+			for {
+				record, err := c.Read()
+				if err == io.EOF {
+					close(ch)
+					return
+				}
+				ch <- record
 			}
+		}()
+	case ext == "txt":
+		scanner := bufio.NewScanner(f)
+		go func() {
+			for scanner.Scan() {
+				var lines []string
+				lines = append(lines, scanner.Text())
+				ch <- lines
+				if err := scanner.Err(); err != nil {
+					fmt.Printf("error for %s: %v\n", file, err)
+					break
+				}
 
-		}
-		close(ch)
-	}()
+			}
+			close(ch)
+		}()
+	}
 
 	return ch, nil
 }
